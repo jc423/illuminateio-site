a3a2b4e11bc9268dd51bdc1b57610ee715d90b37
Joe Crawford <jc423@buffalo.edu>
2019-05-27 23:02:36 -0400

    fix typo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 80;
 
-app.use(express.static('public'))n;
+app.use(express.static('public'));
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
