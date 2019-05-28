c996318c6db040d520de436d9447e3e11b0b3100
Joe Crawford <jc423@buffalo.edu>
2019-05-27 23:05:35 -0400

    take 2 on public files

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ const app = express();
 const port = 80;
 
 app.use(express.static('public'));
-app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
