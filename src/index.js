8076a81082f3a61c22f02e912b53232271e2ded5
Joe Crawford <jc423@buffalo.edu>
2019-05-27 23:01:33 -0400

    move files to public

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 80;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('public'))n;
+app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
